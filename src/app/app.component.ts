d50bc2035e252f8abc360cb127bdb468ccbdfc8d
RacineDuCiel <theogo@live.fr>
2025-10-02 20:04:27 +0200

    replacer le composant home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { BackwardComponent } from './backward/backward.component';
 import { DebugComponent } from './debug/debug.component';
 import { ForwardComponent } from './forward/forward.component';
 import { RefreshComponent } from './refresh/refresh.component';
-import { Home } from './app/home/home';
+import { Home } from './home/home';
 import { CookiesButtonComponent } from './cookies-button/cookies-button.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { BrowserService } from '../../browser.service';
+import { BrowserService } from '../browser.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
